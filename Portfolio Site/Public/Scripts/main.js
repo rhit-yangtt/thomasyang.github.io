6be9930330ac99089756c4938d927556eca9f542
rhit-yangtt <78572332+rhit-yangtt@users.noreply.github.com>
2021-07-01 19:33:52 -0400

    css

--- Portfolio Site/Public/Scripts/main.js
+++ Portfolio Site/Public/Scripts/main.js
@@ -1,5 +1,32 @@
+
+
+const fireworks = new Fireworks({
+    target: document.querySelector('.fireworks'),
+    hue: 120,
+    startDelay: 1,
+    minDelay: 20,
+    maxDelay: 30,
+    speed: 5,
+    acceleration: 1.15,
+    friction: 0.88,
+    gravity: 1,
+    particles: 65,
+    trace: 3,
+    explosion: 6,
+    boundaries: {
+      top: 70,
+      bottom: container.clientHeight,
+      left: 70,
+      right: container.clientWidth
+    },
+})
+
+
+
+
 function main() {
-
+    console.log("Ready");
+    let pictureEffect = document.querySelector()
+    fireworks.start();
 }
-
 main();
