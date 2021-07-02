81eb140d83ef22bbdd6b7d8314db1d0d9a1523b9
rhit-yangtt <78572332+rhit-yangtt@users.noreply.github.com>
2021-07-01 21:54:41 -0400

    javascript

--- Portfolio Site/Public/Scripts/main.js
+++ Portfolio Site/Public/Scripts/main.js
@@ -1,32 +1,21 @@
 
-
-const fireworks = new Fireworks({
-    target: document.querySelector('.fireworks'),
-    hue: 120,
-    startDelay: 1,
-    minDelay: 20,
-    maxDelay: 30,
-    speed: 5,
-    acceleration: 1.15,
-    friction: 0.88,
-    gravity: 1,
-    particles: 65,
-    trace: 3,
-    explosion: 6,
-    boundaries: {
-      top: 70,
-      bottom: container.clientHeight,
-      left: 70,
-      right: container.clientWidth
-    },
-})
-
-
-
-
 function main() {
-    console.log("Ready");
-    let pictureEffect = document.querySelector()
-    fireworks.start();
+    const lightButton = document.querySelector("#lightButton");
+    const darkButton = document.querySelector("#darkButton");
+    let body =  document.querySelector("body");
+    
+    lightButton.onclick = (event) => {
+        body.style.backgroundColor="#F4F6F7";
+        body.style.color="#1B2631";
+        body.style.backgroundImage= "none";
+    }
+
+    darkButton.onclick = (event) => {
+        body.style.backgroundColor="none"
+        body.style.color="#F4F6F7";
+        body.style.backgroundImage= "radial-gradient(circle 5px at top left, rgba(226, 226, 226,0.1) 0%, rgba(226, 226, 226,0.1) 50%,rgba(201, 201, 201,0.1) 50%, rgba(201, 201, 201,0.1) 30%,transparent 30%, transparent 50%),linear-gradient(90deg, rgb(51,51,51),rgb(51,51,51))"; 
+        body.style.backgroundSize= "11px 11px";
+    }
 }
+
 main();
